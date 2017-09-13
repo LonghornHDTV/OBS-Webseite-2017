775d03f804c4ec487e7389155c78f5a116cc297d
LonghornHDTV <31323357+LonghornHDTV@users.noreply.github.com>
2017-09-13 22:06:34 +0200

    Einloggen geht!
    Jetzt kann man sich schon einloggen. Man muss in der MySql eine Datenbank erstellen. Der Standertname ist schoolpage. Dort müsst ihr 2 Spalten an geben, die wie folgt heissen: Username, Password Wenn das getan ist kann man es schon ausprobieren. Viel Spass :)

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
 $MYSQL_HOST = '127.0.0.1'; // Bitte ändern falls der MySQL Server nicht lokal läuft.
 
-$MYSQL_LOGIN_BENUTZER = 'root2'; //Bitte hier der Benutzer der keine Rechte hat an der Datenbank was zu ändern.
-$MYSQL_LOGIN_PASSWORT = 'admin2'; //Bitte hier das Passwort von dem Benutzer der keine Rechne zum ändern hat.
-
-$MYSQL_ADMIN_BENUTZER = 'root'; //Hier bitte der Benutzer der alle Rechte auf die Datenbank hat.
-$MYSQL_ADMIN_PASSWORT = 'admin'; //Hier bitte das Pssswort von dem Benutzer der volle Rechte hat.
+$MYSQL_LOGIN_BENUTZER = 'root2'; //Bitte hier der Benutzer der alle Rechte auf die Datenbank hat.
+$MYSQL_LOGIN_PASSWORT = 'admin2'; //Bitte hier das Passwort von dem Benutzer.
 
 $MYSQL_DATENBANK = 'schoolpage'; //Hier bitte den Datenbank Namen eingeben.
 ?>
--- login.php
+++ login.php
@@ -10,10 +10,96 @@ $conn = mysqli_connect(
 
 mysqli_set_charset($conn, 'utf8');
 
-if($conn) {
-  echo 'Verbindung hergestellt.';
-  print_r($conn);
+if(!$conn->connect_errno) {
+//Validate
+  if(isset($_POST['username'])) {
+    if(isset($_POST['password'])) {
+      //Check Login
+    //  if(!mysqli_select_db($MYSQL_DATENBANK, $conn)) {
+    //    die("<font style=\"color: red;\">Keine Verbindung zur Datenbank.</font>");
+    //  }
+
+      $sql = "SELECT * FROM logins WHERE Username = ? LIMIT 1";
+      $statement = $conn->prepare($sql);
+      if(!$statement) {
+        print_r($conn);
+        die("Query Error with INSERT: " . $conn->prepare($sql) . " : " . $sql . " : ");
+      }
+      $statement->bind_param('s', $username);
+
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      if(!$statement->execute()) {
+        die("Query fehlgeschlagen: " .$statement->error);
+      }
+      $statement->store_result();
+
+      $statement->bind_result($usernamedb, $db_password);
+      $statement->fetch();
+      if($statement->num_rows == 1) {
+        echo "Erfogreich eingeloggt. [DEBUG]: " . $usernamedb . " : " . $db_password;
+      } else{
+        die("Den Benutzer " . $username . " gibt es nicht.");
+      }
+
+    } else {
+        $errorMessage = "Bitte ein Passwort angeben.";
+    }
+  } else {
+    $errorMessage = "Bitte ein Benutzernamen angeben.";
+  }
 } else {
-  die('Keine Verbindung möglich: '.mysqli_connect_error());
+  die('Keine Verbindung möglich: '.$conn->connect_error);
 }
  ?>
+
+ <html lang="de-DE">
+   <head>
+     <meta http-equiv="content-type" content="text/html;charset=utf-8">
+     <title>OBS Badenhausen - Demo</title>
+     <link rel="shortcut icon" href="http://www.obs-badenhausen.de/templates/obs_badenhausen_j25a4107/favicon.ico" type="image/vnd.microsoft.icon">
+     <link rel="icon" href="http://www.obs-badenhausen.de/templates/obs_badenhausen_j25a4107/favicon.ico" type="image/vnd.microsoft.icon">
+     <link rel="stylesheet" type="text/css" media="all" href="css/main.css">
+     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+     <script type="text/javascript" charset="utf-8" src="js/jquery.leanModal.min.js"></script>
+     <!-- Loading Jquery plgin leanModal under MIT License http://leanmodal.finelysliced.com.au/ -->
+   </head>
+   <?php
+   if(isset($errorMessage)) {
+    echo "<body>";
+    echo "<div class=\"errorMessage\"><label>" . $errorMessage . "</label></div>";
+  } else {
+    echo "<body>";
+  }
+   ?>
+     <div id="w">
+       <h1>Welcome to the Demo Test Login Site</h1>
+       <p>Click the login link below to expand the login window. This is only a demo.</p>
+       <center>
+         <a href="#loginmodal" class="flatbtn" id="modaltrigger">
+           <span style="vertical-align: bottom;">Login</span>
+         </a>
+       </center>
+     </div>
+     <div id="loginmodal" style="display: none;">
+       <h1>Benutzer Login</h1>
+       <form id="loginform" name="loginfrom" method="post" action="login.php">
+         <label for="username">Benutzername:</label>
+         <input type="text" name="username" id="username" class="txtfield" tabindex="1">
+
+         <label for="password">Passwort:</label>
+         <input type="password" name="password" id="password" class="txtfield" tabindex="2">
+
+         <div class="center">
+           <input type="submit" name="loginbtn" class="flatbtn-blu hidemodal" value="Log In" tabindex="3">
+         </div>
+       </form>
+     </div>
+     <script type="text/javascript">
+       $(function(){
+         $('#modaltrigger').leanModal({top: 110, overlay: 0.45, closeButton: ".hidemodal"});
+       });
+     </script>
+   </body>
+ </html>
